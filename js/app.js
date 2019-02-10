39b4f83f4b83bee22f0cc846f7f148a10bf12576
arthurhf <arthur.h.f13@gmail.com>
2019-02-10 17:45:53 -0200

    v2

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/* // pego os elementos
+let $input = document.querySelector('input');
+let $h1 = document.querySelector('h1');
+
+// adiciona um evento
+$input.addEventListener('input', adicionaTexto);
+
+// funcao que vai adicionar texto
+function adicionaTexto() {
+  $h1.textContent = $input.value;
+}
+ */
