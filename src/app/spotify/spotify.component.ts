c2b48eaf77e39ab188e324f2ef275053c17740e3
Arthur <arthur.h.f13@gmail.com>
2021-01-11 17:51:54 -0300

    cleared spotify

--- src/app/spotify/spotify.component.ts
+++ src/app/spotify/spotify.component.ts
@@ -17,9 +17,9 @@ export class SpotifyComponent implements OnInit {
   getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
+      q = window.location.hash.substring(1);
+    while (e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
   }
